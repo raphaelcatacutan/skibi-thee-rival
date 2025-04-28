6f91351d59a2be3066a4bd279f8d58b2c05a0750
elbrauq <90502373+elbrauq@users.noreply.github.com>
2025-04-28 15:33:44 +0800

    Added routes for sending and receiving images

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,30 @@
-console.warn("Hello")
+import * as dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import postImage from "./routes/postImage"; 
+import getImage from "./routes/getImage"; 
+const path = require('path');
+
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the API');
+});
+
+// Image uploads
+app.use('/uploads', express.static("./uploads"));
+app.use('/', postImage);
+app.use('/', getImage);
